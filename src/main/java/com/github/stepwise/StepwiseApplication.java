2b400b2f45cf9d592d03b53306728d02f46028a2
garinyaroslav <garinyaroslav13@mail.ru>
2025-07-17 22:41:33 +0300

    initial commit

---
+++ src/main/java/com/github/stepwise/StepwiseApplication.java
@@ -0,0 +1,13 @@
+package com.github.stepwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StepwiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StepwiseApplication.class, args);
+	}
+
+}
